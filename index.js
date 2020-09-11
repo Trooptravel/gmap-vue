e066960d96bd4db08ac85e6383132747e6398fb1
rhyno@landobyte.com <rhyno@landobyte.com>
2020-09-11 12:08:43 +0200

    Added index file to main dir

---
+++ index.js
@@ -0,0 +1,77 @@
+import GmapVue3 from './src/main.js';
+
+console.log('GmapVue3:',GmapVue3);
+
+import {
+  loadGmapApi,
+  install,
+  HeatmapLayer,
+  KmlLayer,
+  Marker,
+  Polyline,
+  Polygon,
+  Circle,
+  Cluster,
+  Rectangle,
+  DrawingManager,
+  InfoWindow,
+  Map,
+  PlaceInput,
+  MapElementMixin,
+  MapElementFactory,
+  Autocomplete,
+  MountableMixin,
+  gmapApi,
+  StreetViewPanorama,
+} from './packages/gmap-vue/src/main.js';
+
+const all_components = {
+  loadGmapApi,
+  install,
+  HeatmapLayer,
+  KmlLayer,
+  Marker,
+  Polyline,
+  Polygon,
+  Circle,
+  Cluster,
+  Rectangle,
+  DrawingManager,
+  InfoWindow,
+  Map,
+  PlaceInput,
+  MapElementMixin,
+  MapElementFactory,
+  Autocomplete,
+  MountableMixin,
+  gmapApi,
+  StreetViewPanorama,
+}
+
+export {
+  loadGmapApi,
+  install,
+  HeatmapLayer,
+  KmlLayer,
+  Marker,
+  Polyline,
+  Polygon,
+  Circle,
+  Cluster,
+  Rectangle,
+  DrawingManager,
+  InfoWindow,
+  Map,
+  PlaceInput,
+  MapElementMixin,
+  MapElementFactory,
+  Autocomplete,
+  MountableMixin,
+  gmapApi,
+  StreetViewPanorama,
+};
+
+export default all_components;
+
+
+
